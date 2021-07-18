00461c065b593fc1a48baa1d97a379e67a0e7353
Santosh Bujala <bujalasanthoshreddy@gmail.com>
2021-07-18 21:29:01 +0530

    Reactive Forms with Validation

--- AngularCompleteRouting/src/app/user/login/login.component.ts
+++ AngularCompleteRouting/src/app/user/login/login.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() {
+  
+  profileForm = this.fb.group({
+    name: [null, [Validators.required, Validators.minLength(8)]],
+    pwd: [null, [Validators.required, Validators.minLength(8)]]
+  });
+  constructor(private fb: FormBuilder) {
     console.log('Login Module loaded');
    }
 
+   get f(){
+    return this.profileForm.controls;
+  }
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
   }
 
 }
