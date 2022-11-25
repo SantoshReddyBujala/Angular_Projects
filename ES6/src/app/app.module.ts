1c551c91851390e0588bede484df037ab6967b91
Santosh Bujala <bujalasanthoshreddy@gmail.com>
2022-11-25 18:37:16 +0530

    Added Features of Json

--- ES6/src/app/app.module.ts
+++ ES6/src/app/app.module.ts
@@ -1,14 +1,26 @@
+import { CommonModule } from '@angular/common'; 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './material.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ExpansionListComponent } from './expansion-list/expansion-list.component';
+import { MatExpansionModule } from '@angular/material';
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule, MaterialModule ],
-  declarations: [ AppComponent, ExpansionListComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MatExpansionModule
+  ],
+  declarations: [AppComponent, ExpansionListComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- ES6/src/app/expansion-list/expansion-list.component.ts
+++ ES6/src/app/expansion-list/expansion-list.component.ts
@@ -3,10 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-expansion-list',
   templateUrl: './expansion-list.component.html',
-  styleUrls: ['./expansion-list.component.less']
+  styleUrls: ['./expansion-list.component.less'],
 })
 export class ExpansionListComponent {
-
-  items: string[] = [ "Item 1", "Item 2", "Item 3", "Item 4"];
-  toggle:any[] = [];
+  toggle: any[] = [];
 }
